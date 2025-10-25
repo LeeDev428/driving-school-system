b60d48dc2cc934feaa4d0496c2e2323d926e131c
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-10-25 14:06:29 +0800

    Delete add_vehicle_type_to_simulation.sql

--- add_vehicle_type_to_simulation.sql
+++
@@ -1,8 +0,0 @@
--- Add vehicle_type column to simulation_results table
-
-ALTER TABLE `simulation_results` 
-ADD COLUMN `vehicle_type` VARCHAR(20) NOT NULL DEFAULT 'car' AFTER `simulation_type`;
-
--- Update existing records to have 'car' as default
-UPDATE `simulation_results` SET `vehicle_type` = 'car' WHERE `vehicle_type` IS NULL;
-
